81c83f8f3a6b8de78d4299b1eeaaa64336a67a14
hkumarmk <hkumarmk@gmail.com>
2015-06-22 11:24:17 +0530

    Add image_format param to tempest::provision

--- manifests/provision.pp
+++ manifests/provision.pp
@@ -7,6 +7,7 @@
 class tempest::provision (
   $image_public     = 'yes',
   $image_source     = 'http://download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img',
+  $image_format     = 'qcow2',
   $imagename        = undef,
   $alt_image_source = undef,
   $alt_imagename    = undef,
@@ -98,13 +99,13 @@ class tempest::provision (
 
 
   if $imagename {
-  glance_image { $imagename:
-    ensure           => present,
-    is_public        => $image_public,
-    container_format => 'bare',
-    disk_format      => 'qcow2',
-    source           => $image_source,
-  }
+    glance_image { $imagename:
+      ensure           => present,
+      is_public        => $image_public,
+      container_format => 'bare',
+      disk_format      => $image_format,
+      source           => $image_source,
+    }
   }
 
   if ($alt_imagename) and  ($alt_image_source) {
